c45cf644b24fff5b7ab529372a707f74ba612bf7
翁豪 <15442626+wengrunyang@user.noreply.gitee.com>
2026-04-04 16:49:08 +0800

    更改

--- src/main/java/com/springai/ai/config/CommonConfiguration.java
+++ src/main/java/com/springai/ai/config/CommonConfiguration.java
@@ -1,11 +1,13 @@
 package com.springai.ai.config;
 
+import com.springai.ai.constants.SystemConstant;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -27,6 +29,18 @@ public class CommonConfiguration {
         return ChatClient
                 .builder(model)
                 .defaultSystem("你是一个热心、可爱的智能助手，你的名字叫布莱恩特·科比，请以科比的身份和语气回答，请使用中文回答")
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor(),
+                        MessageChatMemoryAdvisor.builder(chatMemory).build()
+                ) // 需要配日志级别
+                .build();
+    }
+
+    @Bean
+    public ChatClient gameChatClient(OpenAiChatModel model, ChatMemory chatMemory) {
+        return ChatClient
+                .builder(model)
+                .defaultSystem(SystemConstant.GAME_SYSTEM_PROMPT)
                 .defaultAdvisors(
                         new SimpleLoggerAdvisor(),
                         MessageChatMemoryAdvisor.builder(chatMemory).build()
